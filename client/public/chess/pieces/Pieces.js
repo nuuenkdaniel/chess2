430b5830af654500619726a70b548ee1477dcc1a
Daniel Lew <lewdaniel2@gmail.com>
2024-12-28 23:11:10 -0500

    Finished king pawn and knight

--- client/public/chess/pieces/Pieces.js
+++ client/public/chess/pieces/Pieces.js
@@ -6,12 +6,25 @@ class Piece {
   get_color() { return this.color }
   set_color(color) { this.color = color }
 
+  /**
+   * @param {int[][]} possible_moves - possible_moves of the piece
+   * @returns {int[][]} - the possible_moves that won't cause the king to be checked
+   * @board {Chess_board} board - the chess board
+   */
+  remove_check(possible_moves, board) {
+    let temp_moves = [];
+    for(let i = 0; i < possible_moves.length; i++) {
+      if(board.move_piece()) temp_moves.push(possible_moves[i]);
+    }
+    return temp_moves;
+  }
+
   // Returns all visible tiles of a piece
   get_visible_tiles(r, c, board) { throw new error("Method 'get_visible_tiles(r, c, board)' must be implemented") }
 
   // Returns all the possible moves of a piece
   get_possible_moves(r, c, board) { throw new error("Method 'calc_possible_moves(r, c, board)' must be implemented") }
-  get_type() { throw new error("Method 'get_type()' must be implemented") }
+  get_piece() { throw new error("Method 'get_piece()' must be implemented") }
 }
 
 class Movement_piece extends Piece {
@@ -20,19 +33,21 @@ class Movement_piece extends Piece {
     super(color);
   }
 
-  calc_possible_moves_helper(r, c, move_r, move_c, board) {
+  get_possible_moves_helper(r, c, move_r, move_c, board) {
     let i = 1;
     let temp_r = r+i*move_r;
     let temp_c = c+i*move_c;
+    let possible_moves = [];
     // Keep moving in direction until stopped by piece or bounds
     while((temp_r < board.get_height() && temp_r >= 0) && 
       (temp_c < board.get_width() && temp_c >= 0)) {
       if(board.get_tile(temp_r, temp_c) != null) {
-        if(board.get_tile(temp_r, temp_c).get_color != this.color) this.possible_moves.push([temp_r, temp_c]);
+        if(board.get_tile(temp_r, temp_c).get_color != this.color) possible_moves.push([temp_r, temp_c]);
         break;
       }
-      this.possible_moves.push([temp_r, temp_c]);
+      possible_moves.push([temp_r, temp_c]);
     }
+    return possible_moves;
   }
 }
 
@@ -84,7 +99,7 @@ class King extends Piece {
 
   get_possible_moves(r, c, board) {
     let possible_moves = this.get_visible_tiles;
-    
+
     // Check if visible tiles have pieces on them
     let temp;
     let temp_moves = [];
@@ -93,30 +108,27 @@ class King extends Piece {
       if(temp == null || temp.get_color() != this.color) temp_moves.push(temp);
     }
     possible_moves = temp;
-    
+
     // Check if can castle
-    if(this.get_first_move()) {
+    if(this.get_first_move() && !this.is_checked()) {
       let col_offset = 0;
       // Check castle left
       while(c+col_offset > 0 && board.get_tile(r,c+col_offset) == null) col_offset--;
       if(board.get_tile(r, c+col_offset) != null &&
-        board.get_tile(r, c+col_offset).get_type() == "rook" &&
+        board.get_tile(r, c+col_offset).get_piece() == "rook" &&
         board.get_tile(r, c+col_offset).get_first_move()) possible_moves.push([r,c-2]);
       // Check castle right
       col_offset = 0;
       while(c+col_offset < board.get_length()-1 && board.get_tile(r,c+col_offset) == null) col_offset++;
       if(board.get_tile(r, c+col_offset) != null &&
-        board.get_tile(r, c+col_offset).get_type() == "rook" &&
+        board.get_tile(r, c+col_offset).get_piece() == "rook" &&
         board.get_tile(r, c+col_offset).get_first_move()) possible_moves.push([r,c+2]);
     }
 
     // Check if possible moves causes a check
-    temp_moves = [];
-    for(let i = 0; i < possible_moves.length; i++) {
-      if(board.move_piece()) temp_moves.push(possible_moves[i]);
-    }
+    possible_moves = this.remove_check(possible_moves, board);
 
-    return temp_moves;
+    return possible_moves;
   }
 
   get_first_move() { return this.first_move }
@@ -129,68 +141,97 @@ class Pawn extends Piece {
     super(color);
     this.first_move = true;
   }
+
   get_first_move() { return this.first_move }
   set_first_move(first_move) { this.first_move = first_move }
 
+  get_visible_tiles(r, c, board) {
+    let visible_tiles = [];
+    let move = (this.color == "black")? 1:-1;
+    if(r+move < board.get_length() && r+move >= 0) {
+      if(c-1 >= 0) visible_tiles.push([r+move,c-1]);
+      if(c+1 < board.get_width()) visible_tiles.push([r+move,c+1]);
+    }
+    return visible_tiles;
+  }
+
   /**
-   * Returns all possible moves of the pawn in context of the board
-   * return - An array of possible moves
+   * Returns all possible moves of the pawn in the context of the board
+   * @param {int} r - row on the board the pawn is on
+   * @param {int} c - column on the board the pawn is on
+   * @returns {int[][]} - An array of possible moves
    */
-  calc_possible_moves(r, c, board) {
-    this.possible_moves = [];
+  get_possible_moves(r, c, board) {
+    let possible_moves = [];
     let enpassant = board.get_enpassant();
     let move = (this.color == "black")? 1:-1;
     let temp = null;
 
-    // Check if piece is in front of pawn
-    if(board.get_tile(r+1*move, c) == null) {
-      this.possible_moves.push([r+1*move, c]);
-      // Check if pawns first move
-      if(this.first_move == true && board.get_tile(r+2*move, c) == null) this.possible_moves.push([r+2*move, c]);
-    }
-
-    // Check if pieces are diagnol to the pawn
-    temp = board.get_tile(r+1*move, c+1);
-    if(temp != null && temp.get_color() != this.color) this.possible_moves.push(temp);
-    temp = board.get_tile(r+1*move, c-1);
-    if(temp != null && temp.get_color() != this.color) this.possible_moves.push(temp);
-
-    // Check if enpassant is possible
-    if(enpassant.length != 0 && enpassant[0] == r && Math.abs(enpassant[1]-c) == 1) this.possible_moves.push([r+1*move, enpassant[1]]); 
-
-    // Removing out of bounds moves and conflicting pieces
-    let returned_moves = [];
-    for(let i = 0; i < this.possible_moves; i++) {
-      temp = this.possible_moves.pop();
-      if(temp[0] < board.get_height() && temp[0] >= 0 && temp[1] < board.get_width() && temp[1] >= 0) {
-        if(board.get_tile(temp[0], temp[1]) == null || board.get_tile(temp[0], temp[1]).get_color() != this.color) returned_moves.push(temp);
+    // Check if tile in front of pawn is within bounds
+    if(r+move < board.get_length() && r+move >= 0) {
+      // Check if tile in front of pawn has a piece
+      if(board.get_tile(r+move, c) == null) {
+        possible_moves.push([r+move, c]);
+        // Check if pawn can move 2
+        if(this.first_move == true &&
+          r+2*move < board.get_length() && r+2*move >= 0 &&
+          board.get_tile(r+2*move, c) == null) possible_moves.push([r+2*move, c]);
       }
+
+      // Check if pieces are diagnol to the pawn
+      temp = (c-1 >= 0)? board.get_tile(r+move, c-1):null;
+      if(temp != null && temp.get_color() != this.color) possible_moves.push(temp);
+      temp = (c+1 < board.get_width())? board.get_tile(r+move, c+1):null;
+      if(temp != null && temp.get_color() != this.color) possible_moves.push(temp);
+
+      // Check if enpassant is possible
+      if(enpassant.length != 0 && enpassant[0] == r && Math.abs(enpassant[1]-c) == 1) possible_moves.push([r+1*move, enpassant[1]]); 
     }
-    this.possible_moves = returned_moves;
-    return returned_moves;
+
+    // Check if possible moves causes a check before returning
+    return this.remove_check(possible_moves, board);
   }
-  get_type() { return "pawn" }
+
+  get_piece() { return "pawn" }
 }
 
 // Knight Class
 class Knight extends Piece {
   constructor(color) { super(color) }
 
-  calc_possible_moves(r, c, board) {
-    this.possible_moves = [[-1,-2],[1,-2],[2,-1],[2,1],[1,2],[-1,2],[-2,1],[-2,-1]];
-    // Check if moves are out of bounds or conflict with another piece
+  get_visible_tiles(r, c, board) {
+    let visible_tiles = [[-1,-2],[1,-2],[2,-1],[2,1],[1,2],[-1,2],[-2,1],[-2,-1]];
     let temp;
-    let returned_moves = [];
+    let returned_tiles = [];
+
     for(let i = 0; i < this.possible_moves.length; i++) {
       temp = this.possible_moves.pop();
       if(temp[0] < board.get_height() && temp[0] >= 0 &&
         temp[1] < board.get_width() && temp[1] >= 0) {
-        if(board.get_tile(temp[0], temp[1]) == null) returned_moves.push([temp[0]+r, temp[0]+c]);
+        returned_tiles.push([temp[0]+r, temp[0]+c]);
       }
     }
+    return returned_tiles;
   }
 
-  get_type() { return "knight" }
+  get_possible_moves(r, c, board) {
+    let possible_moves = this.get_visible_tiles(r, c, board);
+
+    // Check if conflict with another piece
+    let temp;
+    let temp_tile;
+    let returned_moves = [];
+    for(let i = 0; i < this.possible_moves.length; i++) {
+      temp = this.possible_moves[i];
+      temp_tile = board.get_tile(temp[0], temp[1]);
+      if(temp_tile == null || temp_tile.get_color() != this.color) returned_moves.push([temp[0], temp[1]]);
+    }
+
+    // Check if possible moves causes check before returning
+    return this.remove_check(returned_moves, board);
+  }
+
+  get_piece() { return "knight" }
 }
 
-module.exports = { Piece, Movement_piece, Pawn };
+module.exports = { Piece, Movement_piece, Pawn, Knight };
